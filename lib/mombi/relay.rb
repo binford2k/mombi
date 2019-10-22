06b19bc9e346756e3947021ac1fb2ee4580b3a3a
Ben Ford <ben.ford@puppetlabs.com>
2019-10-22 10:24:20 -0700

    initial gpio work
    This is commented out until I get the board and can run it for real.

--- lib/mombi/relay.rb
+++ lib/mombi/relay.rb
@@ -1,25 +1,53 @@
 require 'yaml'
+# require 'rpi_gpio'
+
+PIN_NUM = 15
 
 class Mombi::Relay
-  def initialize(configfile)
+  def initialize(configfile, debug=false)
     @configfile = configfile
-    reload
+    @config     = YAML.load_file(@configfile) rescue {}
+    @debug      = debug
+    setup_gpio
 
     Thread.new do
       loop do
         sleep @config[:interval]
-        puts "futzing for #{@config[:duration]} seconds."
+        turn_on(@config[:duration])
       end
     end
 
   end
 
-  def reload
-    @config = YAML.load_file(@configfile) rescue {}
+  def save(params={})
+    params.each do |key, value|
+      key   = key.to_sym
+      value = value.to_i if [:interval, :duration].include? key
+
+      @config[key] = value
+    end
+    File.write(@configfile, @config.to_yaml)
   end
 
   def blast
     puts 'blasting'
+  end
+
+  def setup_gpio
+#     RPi::GPIO.set_numbering :board
+#     RPi::GPIO.setup PIN_NUM, :as => :output
+  end
+
+  def turn_on(duration=5)
+    debug("Starting fogger for #{duration} seconds.")
+#     RPi::GPIO.set_high PIN_NUM
+    sleep duration
+#     RPi::GPIO.set_low PIN_NUM
+    debug("Stopping fogger.")
+  end
+
+  def debug(message)
+    puts message if @debug
   end
 
 end
